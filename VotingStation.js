cbffb55b2dae8e1323b46bdad17d193ff0d65e67
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-09-02 19:37:34 -0400

    Challenge File

---
+++ VotingStation.js
@@ -0,0 +1,14 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+
+
+const chooseStations = function (stations) {
+  
+
+ console.log(chooseStations);
+
+}
