3d4157c8f920ab360e620c6a9b52d0fc596ddf7e
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-09-02 19:36:38 -0400

    Loopy Light House Challenge

---
+++ LoopyLightHouse.js
@@ -0,0 +1,12 @@
+let number = 200;
+for (let i = 100; i <= number; i++) {
+  if((i % 3 === 0) && (i % 4 === 0)) {
+    console.log("LoopyLighthouse");
+  } else if((i % 3) === 0){
+    console.log("Loopy");
+  } else if((i % 4) === 0){
+    console.log("Lighthouse");
+  } else {
+    console.log(i);
+  }
+}
